554495a9259a395967ce45be88c584036e735f4b
radionadejda <radionadejda@gmail.com>
2023-08-04 14:04:36 +0300

    add date

--- itg_week_12/name_transform/name.js
+++ itg_week_12/name_transform/name.js
@@ -10,8 +10,8 @@ let print = () => {
     let result = input.value.trim();
     result = result.replace(/\s\s+/g, ' ');
     const patternName = /^([а-яА-Я- ])+$/;
-    const patternDouble = /^([а-яА-Я-])+$/;
-    // пробел в паттерне нужен, чтобы учитывать двойные имена (Анна-Мария), сложные отчества ( ...-оглы), двойные фамилии (Салтыков-Щедрин), но как учесть имена без дефиса это вопрос
+    // const patternDouble = /^([а-яА-Я-])+$/;
+    // дефис в паттерне нужен, чтобы учитывать двойные имена (Анна-Мария), сложные отчества ( ...-оглы), двойные фамилии (Салтыков-Щедрин), но как учесть имена с пробелом без дефиса это вопрос
     if (!patternName.test(result)) {
         message.innerHTML = "вводимые данные могут содержать только буквы кириллицы и дефис";
     } else {
@@ -19,18 +19,16 @@ let print = () => {
         if (result.length != 3) {
             message.innerHTML = "введите фамилию, имя и отчество через пробел";
         } else {
-            let lastName = result[0];
-            let firstName = result[1];
-            let middleName = result[2];
-            firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
-            resultFirstName.textContent += " " + firstName;
-            middleName = middleName.charAt(0).toUpperCase() + middleName.slice(1).toLowerCase();
-            resultMiddleName.textContent += " " + middleName;
-            lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
-            resultLastName.textContent += " " + lastName;
+            for (let i = 0; i <= result.lenth; i++) {
+                result[i] =  result[i].charAt(0).toUpperCase() + result[i].slice(1).toLowerCase();
+            };
+            resultLastName.textContent += " " + result[0];
+            resultFirstName.textContent += " " + result[1];
+            resultMiddleName.textContent += " " + result[2];
             input.value = "";
         }
-        // if (patternDouble.test(result[0])) {
+        //здесь может быть else if !patternDouble.test(result[i]) и код на капитализацию имен без дефисов
+        //if (patternDouble.test(result[0])) {
         //         let firstName = result[0];
         //         firstName = firstName.split("-");
         //         test.innerHTML = "вы должно быть из испании!";
@@ -41,6 +39,6 @@ let print = () => {
         //             });
         //         test.innerHTML = firstName.join("-");
     }
-}
+};
 
 button.addEventListener("click", print);
