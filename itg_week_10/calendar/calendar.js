3c54c6f9633b9aef09eb43000f2cfed34534b42c
radionadejda <radionadejda@gmail.com>
2023-08-09 10:45:56 +0300

    edit calc2 and calendar

--- itg_week_10/calendar/calendar.js
+++ itg_week_10/calendar/calendar.js
@@ -1,18 +1,23 @@
-const button = document.querySelector(#button);
-const answer = document.querySelector(#answer);
+const button = document.querySelector("#button");
+const answer = document.querySelector("#answer");
 
 const isLeapYear = () => {
- const year = Number(document.querySelector(#year).value);
- let message = `Find out!`
- if (year%4 === 0) {
-    if (year%100 === 0) {
-      if (year%400 === 0) {
-         message = `${year} is a leap year and has 366 days`
-      } else {message = `${year} is not a leap year and has 365 days`}
-    } else {message = `${year} is a leap year and has 366 days`} 
- } else {message = `${year} is not a leap year and has 365 days`};
- answer.innerHTML = message;
-}
+	const year = Number(document.querySelector("#year").value);
+	let message = `узнать`;
+	if (year % 4 === 0) {
+		if (year % 100 === 0) {
+			if (year % 400 === 0) {
+				message = `${year} - високосный год и в нем 366 дней`;
+			} else {
+				message = `${year} - не високосный год и в нем 365 дней`;
+			}
+		} else {
+			message = `${year} - високосный год и в нем 366 дней`;
+		}
+	} else {
+		message = `${year} - не високосный год и в нем 365 дней`;
+	}
+	answer.innerHTML = message;
+};
 
-button.addEventListener('click', isLeapYear);
-
+button.addEventListener("click", isLeapYear);
--- itg_week_11/calculator/calc2.js
+++ itg_week_11/calculator/calc2.js
@@ -1,55 +1,57 @@
-  let number1 = Number(document.getElementById("number1"));
-  let number2 = Number(document.getElementById("number2"));
-  
-  const sum = function() {
-  
-  let result = Number(number1.value) + Number(number2.value);
-  let calc = document.getElementById("calc");
-  let calcResult = document.createElement("p");
-  calcResult.innerHTML = result;
-  calc.append(calcResult);
+
+// вариант 1 - можно взять элемент за пределами функции, а получить значение и преобразовать его в число внутри функции. будет меньше строк кода, но если что-то поменяется в переменных number1 number2 вне функций, то поедет всё.
+let number1 = document.getElementById("number1");
+let number2 = document.getElementById("number2");
+
+const sum = function() {
+	let result = Number(number1.value) + Number(number2.value);
+	let calc = document.getElementById("calc");
+	let calcResult = document.createElement("p");
+	calcResult.innerHTML = result;
+	calc.append(calcResult);
 };
 document.querySelector("#sum").onclick = sum;
 
+// вариант 2 - взять элемент и сразу получить его значение и преобразовать его в число внутри функции. это +2 строки кода внутри каждой функции, но зато изменить переменные можно только внутри функции.
 const subtract = function() {
-  let number1 = Number(document.getElementById("number1").value);
-  let number2 = Number(document.getElementById("number2").value);
-  let result = number1 - number2;
-  let calc = document.getElementById("calc");
-  let calcResult = document.createElement("p");
-  calcResult.innerHTML = result;
-  calc.append(calcResult);
+	let number1 = Number(document.getElementById("number1").value);
+	let number2 = Number(document.getElementById("number2").value);
+	let result = number1 - number2;
+	let calc = document.getElementById("calc");
+	let calcResult = document.createElement("p");
+	calcResult.innerHTML = result;
+	calc.append(calcResult);
 };
 document.querySelector("#subtract").onclick = subtract;
 
 const multiply = function() {
-  let number1 = Number(document.getElementById("number1").value);
-  let number2 = Number(document.getElementById("number2").value);
-  let result = number1 * number2;
-  let calc = document.getElementById("calc");
-  let calcResult = document.createElement("p");
-  calcResult.innerHTML = result;
-  calc.append(calcResult);
+	let number1 = Number(document.getElementById("number1").value);
+	let number2 = Number(document.getElementById("number2").value);
+	let result = number1 * number2;
+	let calc = document.getElementById("calc");
+	let calcResult = document.createElement("p");
+	calcResult.innerHTML = result;
+	calc.append(calcResult);
 };
 document.querySelector("#multiply").onclick = multiply;
 
 const divide = function() {
-  let number1 = Number(document.getElementById("number1").value);
-  let number2 = Number(document.getElementById("number2").value);
-  let result;
-  if (number2 === 0) {
-    result = "На ноль делить нельзя!";
-  } else {
-    result = number1 / number2;
-  };
-  let calc = document.getElementById("calc");
-  let calcResult = document.createElement("p");
-  calcResult.innerHTML = result;
-  calc.append(calcResult);
+	let number1 = Number(document.getElementById("number1").value);
+	let number2 = Number(document.getElementById("number2").value);
+	let result;
+	if (number2 === 0) {
+		result = "На ноль делить нельзя!";
+	} else {
+		result = number1 / number2;
+	}
+	let calc = document.getElementById("calc");
+	let calcResult = document.createElement("p");
+	calcResult.innerHTML = result;
+	calc.append(calcResult);
 };
 document.querySelector("#divide").onclick = divide;
 
 const clear = () => {
-  location.reload();
+	location.reload();
 };
-document.querySelector('#clear').onclick = clear;
+document.querySelector("#clear").onclick = clear;
