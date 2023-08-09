ebd78035b9cdb2f5f0acf9ffe7d7fd705859710f
radionadejda <radionadejda@gmail.com>
2023-08-09 11:41:51 +0300

    fix calc

--- itg_week_10/calculator/calc.js
+++ itg_week_10/calculator/calc.js
@@ -1,7 +1,10 @@
 const sum = function() {
-	let number1 = Number(prompt("введите первое слагаемое", 1));
-	let number2 = Number(prompt("введите второе слагаемое", 1));
-	let result = number1 + number2;
+	let number1 = prompt("введите первое слагаемое", 1);
+	let number2 = prompt("введите второе слагаемое", 1);
+	if (number1 === "" || number2 === "" || number1.includes(" ") || number2.includes(" ")) {
+		alert("что-то пошло не так ☹, введите два числа");
+	} else {
+	let result = Number(number1) + Number(number2);
 	if (isNaN(result)) {
 		alert("что-то пошло не так ☹, попробуйте еще раз");
 	} else {
@@ -9,12 +12,16 @@ const sum = function() {
 		confirm(`вы согласны?`);
 		alert(`Вы так умны!`);
 	}
+}
 };
 
 const subtract = function() {
-	let number1 = Number(prompt("введите уменьшаемое", 1));
-	let number2 = Number(prompt("введите вычитаемое", 1));
-	let result = number1 - number2;
+	let number1 = prompt("введите уменьшаемое", 1);
+	let number2 = prompt("введите вычитаемое", 1);
+	if (number1 === "" || number2 === "" || number1.includes(" ") || number2.includes(" ")) {
+		alert("что-то пошло не так ☹, введите два числа");
+	} else {
+	let result = Number(number1) - Number(number2);
 	if (isNaN(result)) {
 		alert("что-то пошло не так ☹, попробуйте еще раз");
 	} else {
@@ -22,12 +29,16 @@ const subtract = function() {
 		confirm(`вы согласны?`);
 		alert(`Вы умничка!`);
 	}
+}
 };
 
 const multiply = function() {
-	let number1 = Number(prompt("введите первый множитель", 1));
-	let number2 = Number(prompt("введите второй множитель", 1));
-	let result = number1 * number2;
+	let number1 = prompt("введите первый множитель", 1);
+	let number2 = prompt("введите второй множитель", 1);
+	if (number1 === "" || number2 === "" || number1.includes(" ") || number2.includes(" ")) {
+		alert("что-то пошло не так ☹, введите два числа");
+	} else {
+	let result = Number(number1) * Number(number2);
 	if (isNaN(result)) {
 		alert("что-то пошло не так ☹, попробуйте еще раз");
 	} else {
@@ -35,12 +46,16 @@ const multiply = function() {
 		confirm(`вы согласны?`);
 		confirm(`Вы умничка?`);
 	}
+}
 };
 
 const divide = function() {
-	let number1 = Number(prompt("введите делимое", 1));
-	let number2 = Number(prompt("введите делитель", 1));
-	let result = number1 / number2;
+	let number1 = prompt("введите делимое", 1);
+	let number2 = prompt("введите делитель", 1);
+	if (number1 === "" || number2 === "" || number1.includes(" ") || number2.includes(" ")) {
+		alert("что-то пошло не так ☹, введите два числа");
+	} else {
+	let result = Number(number1) / Number(number2);
 	if (isNaN(result)) {
 		alert("что-то пошло не так ☹, попробуйте еще раз");
 	} else {
@@ -48,4 +63,5 @@ const divide = function() {
 		confirm(`вы согласны?`);
 		alert(`Вы умничка!`);
 	}
+}
 };
