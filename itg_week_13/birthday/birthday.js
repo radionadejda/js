0c006df123f57d1ad1dbdae454313e04acfd44a2
radionadejda <radionadejda@gmail.com>
2023-08-11 10:13:40 +0300

    add invalid date error

--- itg_week_13/birthday/birthday.js
+++ itg_week_13/birthday/birthday.js
@@ -1,29 +1,56 @@
 const button = document.querySelector('#button');
 const resultDate = document.querySelector('#formatted');
-const input = document.querySelector('#datetime');
+const error = document.querySelector('#error');
+const input = document.querySelector('#birthday');
 
-const formatDate = (inputDate) => {
-    inputDate = new Date (input.value);
+const formatDate = () => {
+    const inputDate = new Date(input.value);
     const currentDate = new Date();
+    const oneDay = 1000 * 60 * 60 * 24;
     let timeDifference = 0;
-    if ((inputDate - currentDate)>=1000) {
+    let message = '';
+    inputDate.setHours(0, 0, 0, 0);
+    currentDate.setHours(0, 0, 0, 0);
+    if (!input.value) {
+        error.textContent = "укажите дату дня рождения";
+        error.style.color = 'red';
+        return;
+    } else if (inputDate > currentDate) {
         timeDifference = inputDate - currentDate;
-        message = "до события осталось ";
-    } else if ((currentDate - inputDate)>=1000) {
+        message = "до дня рождения осталось ";
+    } else if (inputDate < currentDate) {
         timeDifference = currentDate - inputDate;
-        message = "после события прошло ";
+        message = "после дня рождения прошло ";
     } else {
-        message = "прямо сейчас";
+        message = "сегодня день рождения!";
         resultDate.textContent = message;
         return;
     }
-    let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+        const days = Math.floor(timeDifference / oneDay);
 
-    resultDate.textContent = message + days + " дней, " + hours + " часов, " + minutes + " минут, " + seconds + " секунд.";
-    input.value = "2023-08-04T14:00";
+
+    // function dayTitle(number) {
+    //     if (number > 10 && [11, 12, 13, 14].includes(number%100)) return 'дней';
+    //     last_num = number%10;
+    //     if (last_num == 1) return 'день';
+    //     if ([2,3,4].includes(last_num)) return 'дня';
+    //     if ([5,6,7,8,9, 0].includes(last_num)) return 'дней';
+    // }
+
+    // добавляет новый параграф с каждым результатом
+    // const newResult = document.createElement("p");
+	// newResult.textContent = input.value;
+	// resultDate.append(newResult);
+    // newResult.textContent = message + days + " дней ";
+
+    // перезаписывает результат в один параграф
+    resultDate.textContent = message + days + " дней ";
+
 };
 
 button.addEventListener("click", formatDate);
+
+const clear = () => {
+    location.reload();
+};
+document.querySelector('#clear').onclick = clear;
