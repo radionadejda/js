10f8603eb21126859c464da20a34c582c4df45e4
radionadejda <radionadejda@gmail.com>
2023-08-11 11:47:12 +0300

    add date format to birthday

--- itg_week_13/birthday/birthday.js
+++ itg_week_13/birthday/birthday.js
@@ -1,56 +1,74 @@
-const button = document.querySelector('#button');
-const resultDate = document.querySelector('#formatted');
-const error = document.querySelector('#error');
-const input = document.querySelector('#birthday');
+const button = document.querySelector("#button");
+const resultDate = document.querySelector("#formatted");
+const error = document.querySelector("#error");
+const input = document.querySelector("#birthday");
 
 const formatDate = () => {
-    const inputDate = new Date(input.value);
-    const currentDate = new Date();
-    const oneDay = 1000 * 60 * 60 * 24;
-    let timeDifference = 0;
-    let message = '';
-    inputDate.setHours(0, 0, 0, 0);
-    currentDate.setHours(0, 0, 0, 0);
-    if (!input.value) {
-        error.textContent = "укажите дату дня рождения";
-        error.style.color = 'red';
-        return;
-    } else if (inputDate > currentDate) {
-        timeDifference = inputDate - currentDate;
-        message = "до дня рождения осталось ";
-    } else if (inputDate < currentDate) {
-        timeDifference = currentDate - inputDate;
-        message = "после дня рождения прошло ";
-    } else {
-        message = "сегодня день рождения!";
-        resultDate.textContent = message;
-        return;
-    }
-        const days = Math.floor(timeDifference / oneDay);
+	const inputDate = new Date(input.value);
+	const currentDate = new Date();
+	const oneDay = 1000 * 60 * 60 * 24;
+	let timeDifference = 0;
+	let message = "";
+	let formatDay = "";
+	inputDate.setHours(0, 0, 0, 0);
+	currentDate.setHours(0, 0, 0, 0);
 
+	if (!input.value) {
+		error.textContent = "укажите дату дня рождения";
+		error.style.color = "red";
+		return;
+	}
 
-    // function dayTitle(number) {
-    //     if (number > 10 && [11, 12, 13, 14].includes(number%100)) return 'дней';
-    //     last_num = number%10;
-    //     if (last_num == 1) return 'день';
-    //     if ([2,3,4].includes(last_num)) return 'дня';
-    //     if ([5,6,7,8,9, 0].includes(last_num)) return 'дней';
-    // }
+	if (inputDate > currentDate) {
+		timeDifference = inputDate - currentDate;
+		let days = Math.floor(timeDifference / oneDay);
+		message = "до дня рождения ";
+		last_num = days % 10;
+		days = Math.abs(days);
+		if (days > 10 && [11, 12, 13, 14].includes(days % 100)) {
+			formatDay = "осталось " + days + " дней";
+		} else if (last_num === 1) {
+			formatDay = "осталcя " + days + " день";
+		} else if ([2, 3, 4].includes(last_num)) {
+			formatDay = "осталось " + days + " дня";
+		} else if ([5, 6, 7, 8, 9, 0].includes(last_num)) {
+			formatDay = "осталось " + days + " дней";
+		}
+	} else if (inputDate < currentDate) {
+		timeDifference = currentDate - inputDate;
+		let days = Math.floor(timeDifference / oneDay);
+		message = "после дня рождения ";
+		last_num = days % 10;
+		days = Math.abs(days);
+		if (days > 10 && [11, 12, 13, 14].includes(days % 100)) {
+			formatDay = "прошло " + days + " дней";
+		} else if (last_num === 1) {
+			formatDay = "прошел " + days + " день";
+		} else if ([2, 3, 4].includes(last_num)) {
+			formatDay = "прошло " + days + " дня";
+		} else if ([5, 6, 7, 8, 9, 0].includes(last_num)) {
+			formatDay = "прошло " + days + " дней";
+		}
+	} else {
+		message = "сегодня день рождения!";
+		resultDate.textContent = message;
+		return;
+	}
 
-    // добавляет новый параграф с каждым результатом
-    // const newResult = document.createElement("p");
-	// newResult.textContent = input.value;
-	// resultDate.append(newResult);
-    // newResult.textContent = message + days + " дней ";
+	// перезаписывает результат в один параграф
+	resultDate.textContent = message + formatDay;
 
-    // перезаписывает результат в один параграф
-    resultDate.textContent = message + days + " дней ";
-
+    // добавляет новый параграф с каждым новым результатом
+// const newResult = document.createElement("p");
+// newResult.textContent = input.value;
+// resultDate.append(newResult);
+// newResult.textContent = message + days + " дней ";
 };
 
 button.addEventListener("click", formatDate);
 
 const clear = () => {
-    location.reload();
+	location.reload();
 };
-document.querySelector('#clear').onclick = clear;
+document.querySelector("#clear").onclick = clear;
+
