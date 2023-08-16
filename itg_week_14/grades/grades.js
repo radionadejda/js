69cb215cbef3b8f954d13e5e40fed7c009c7ebbe
radionadejda <radionadejda@gmail.com>
2023-08-16 14:28:57 +0300

    add grades

---
+++ itg_week_14/grades/grades.js
@@ -0,0 +1,30 @@
+const grades = [];
+for (let i = 0; i < 12; i++) {
+	const randomGrade = Math.floor(Math.random() * 100) + 1;
+	grades.push(randomGrade);
+}
+
+document.getElementById("showResults").addEventListener("click", function() {
+	const averageGrade = grades.reduce((sum, grade) => sum + grade, 0) / grades.length;
+	const maxGrade = Math.max(...grades);
+	const minGrade = Math.min(...grades);
+	const positiveGradesCount = grades.filter(grade => grade >= 60).length;
+	const negativeGradesCount = grades.filter(grade => grade < 60).length;
+
+	const letterGrades = grades.map(grade => {
+		if (grade >= 80) return "A";
+		if (grade >= 60) return "B";
+		if (grade >= 40) return "C";
+		if (grade >= 20) return "D";
+		return "E";
+	});
+
+	const resultsDiv = document.getElementById("results");
+	resultsDiv.innerHTML =
+    `<p>Средняя оценка: ${averageGrade.toFixed(2)}</p>
+    <p>Максимальная оценка: ${maxGrade}</p>
+    <p>Минимальная оценка: ${minGrade}</p>
+    <p>Количество положительных отметок: ${positiveGradesCount}</p>
+    <p>Количество отрицательных отметок: ${negativeGradesCount}</p>
+    <p>Буквенные обозначения отметок: ${letterGrades.join(", ")}</p>`;
+});
