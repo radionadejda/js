9390eb2c26b31914be0de003c17e17ae515a962f
radionadejda <radionadejda@gmail.com>
2023-08-19 15:37:58 +0300

    add li and empty input check

--- itg_week_11/todo_list/todo.js
+++ itg_week_11/todo_list/todo.js
@@ -3,11 +3,15 @@ const list = document.querySelector("#todo__list");
 const input = document.querySelector("#task");
 
 const addTask = () => {
-	const newTask = document.createElement("p");
+	const newTask = document.createElement("li");
 	newTask.classList.add("new-task");
+	if (input.value === "") {
+		alert(`введите задание`)
+	} else {
 	newTask.textContent = input.value;
 	list.append(newTask);
 	input.value = "";
+	}
 };
 
 document.querySelector("#button-add").onclick = addTask;
