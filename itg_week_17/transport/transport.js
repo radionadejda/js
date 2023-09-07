5422a32339823a14e0c2f7ff2ddde31d6d9101a9
radionadejda <radionadejda@gmail.com>
2023-09-07 16:05:33 +0300

    fix transport

--- itg_week_17/transport/transport.js
+++ itg_week_17/transport/transport.js
@@ -35,66 +35,98 @@ const data = [
 		maxSpeed: 220,
 		price: 1400000,
 		image: "https://cdn.dealerspike.com/imglib/products/harley-showroom/2020/livewire/main/Vivid-Black-Main.png"
-	}
+	},
+	// это чтобы проверить работает ли default
+	// {
+	// 	id: 5,
+	// 	brand: "Harley-Davidson",
+	// 	maxSpeed: 220,
+	// 	price: 1400000,
+	// 	image: "https://png.pngtree.com/thumb_back/fw800/background/20230521/pngtree-d-green-bike-clipping-jpg-frame-of-a-swanky-green-electric-image_2665813.jpg"
+	// }
 ];
 
 class Transport {
-	constructor(type, price, brand) {
-		(this.type = type), (this.price = price), (this.brand = brand);
+	constructor(type, price, brand, image) {
+		this.type = type,
+		this.price = price,
+		this.brand = brand,
+		this.image = image
 	}
 
 	getInfo() {
 		let info = `${this.type} ${this.brand}`;
-		console.log(info);
+		// console.log(info);
 		return info;
 	}
 
 	getPrice() {
 		let price = `${this.type} ${this.brand} стоит ${this.price}`;
-		console.log(price);
-		return this.price;
+		// console.log(price);
+		return price;
 	}
 }
 
 class Car extends Transport {
-	constructor(type, price, brand, doors) {
-		super(type, price, brand, doors);
+	constructor(type, price, brand, image, doors) {
+		super(type, price, brand, image, doors);
 		this.doors = doors;
 	}
 	getDoorsCount() {
 		let doors = `автомобиль ${this.brand} имеет ${this.doors} двери`;
-		console.log(doors);
-		return this.doors;
+		// console.log(doors);
+		return doors;
 	}
 }
 class Bike extends Transport {
-	constructor(type, price, brand, maxSpeed) {
-		super(type, price, brand);
+	constructor(type, price, brand, image, maxSpeed) {
+		super(type, price, brand, image);
 		this.maxSpeed = maxSpeed;
 	}
 
 	getMaxSpeed() {
-		let speed = `Максимальная скорость мотоцикла ${this.brand}: ${this.maxSpeed}`;
-		console.log(speed);
-		return this.maxSpeed;
+		let speed = `Максимальная скорость мотоцикла ${this.brand}: ${this.maxSpeed} км/ч`;
+		// console.log(speed);
+		return speed;
 	}
 }
 
+const addVehicle = () => {
 data.forEach(item => {
-	if (item.type === "car") {
-		let newCar = new Car("автомобиль", item.price, item.brand, item.doors);
-		newCar.getInfo();
-		newCar.getDoorsCount();
-		newCar.getPrice();
-	} else if (item.type === "bike") {
-		let newBike = new Bike("мотоцикл", item.price, item.brand, item.maxSpeed);
-		newBike.getInfo();
-		newBike.getMaxSpeed();
-		newBike.getPrice();
-	} else {
+	const catalogue = document.getElementById("catalogue");
+	const newVehicle = document.createElement("div");
+	newVehicle.classList.add("vehicle");
+	catalogue.appendChild(newVehicle);
+	const newImage = document.createElement("img");
+	newImage.classList.add("image");
+	const newTitle = document.createElement("h2");
+	newTitle.classList.add("title");
+	const newPrice = document.createElement("div");
+	const newDescription = document.createElement("div");
+	newVehicle.append(newImage, newTitle, newPrice, newDescription);
+	switch (item.type) {
+	case "car":
+		let newCar = new Car("автомобиль", item.price, item.brand, item.image, item.doors);
+		newImage.src = newCar.image;
+		newTitle.textContent = newCar.getInfo();
+		newDescription.textContent = newCar.getDoorsCount();
+		newPrice.textContent = newCar.getPrice();
+		break;
+	case "bike":
+		let newBike = new Bike("мотоцикл", item.price, item.brand, item.image, item.maxSpeed);
+		newImage.src = newBike.image;
+		newTitle.textContent = newBike.getInfo();
+		newDescription.textContent = newBike.getMaxSpeed();
+		newPrice.textContent = newBike.getPrice();
+		break;
+	default:
 		// на тот случай, если в массиве появится неопределенный элемент
-        let newTransport = new Transport("транспортное средство", item.price, item.brand);
-		newTransport.getInfo();
-		newTransport.getPrice();
-	}
-});
+        let newTransport = new Transport("транспортное средство", item.price, item.brand, item.image);
+		newImage.src = newTransport.image;
+		newTitle.textContent = newTransport.getInfo();
+		newPrice.textContent = newTransport.getPrice();
+		break;
+}
+})
+};
+document.body.onload = addVehicle;
