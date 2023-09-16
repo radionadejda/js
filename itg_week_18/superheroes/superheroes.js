e784b108fc453811d43ee72640aa7bcb4e30f212
radionadejda <radionadejda@gmail.com>
2023-09-16 17:10:30 +0300

    add superheroes

--- itg_week_18/superheroes/superheroes.js
+++ itg_week_18/superheroes/superheroes.js
@@ -1,11 +1,9 @@
 // Сделайте страничку-галерею, где можно будет просматривать информацию о супер-героях и ставить им оценки.
-
 // Что должна уметь страничка:
-
 // - показать информацию о героях: картинки, характеристики, подробное описание
 // - поставить герою личную оценку и сохранить значение в localStorage
 
-[
+const heroes = [
 	{
 		name: "Бетмен",
 		universe: "DC Comics",
@@ -118,136 +116,104 @@
 	}
 ];
 
-
-// 2. Вы устроились разработчиком в автомобильную компанию. Вам нужно написать код, который отвечает за отображение на сайте информации о транспорте и цене. Вам нужно:
-// - создать класс Transport, у него есть свойства: type, price, brand и два метода `getInfo()` и `getPrice()` ;
-// - создать класс Car, который наследует от Transport. Этот класс должен содержать метод `getDoorsCount()` , который возвращает количество дверей;
-// - создать класс Bike, который наследует от Transport. Этот класс должен содержать метод `getMaxSpeed()`, который возвращает максимальную скорость мотоцикла.
-
-const data = [
-	{
-		id: 1,
-		type: "car",
-		brand: "Audi",
-		doors: 4,
-		price: 4300000,
-		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/2020_Audi_e-Tron_Sport_50_Quattro.jpg/1200px-2020_Audi_e-Tron_Sport_50_Quattro.jpg"
-	},
-	{
-		id: 2,
-		type: "car",
-		brand: "Mercedes-Benz",
-		doors: 4,
-		price: 2800000,
-		image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg/300px-2019_Mercedes-Benz_E220d_SE_Automatic_2.0_Front.jpg"
-	},
-	{
-		id: 3,
-		type: "bike",
-		brand: "Harley-Davidson",
-		maxSpeed: 210,
-		price: 1300000,
-		image: "https://www.harley-davidson.com/content/dam/h-d/images/product-images/bikes/motorcycle/2022/2022-iron-883/2022-iron-883-016/2022-iron-883-016-motorcycle.jpg"
-	},
-	{
-		id: 4,
-		type: "bike",
-		brand: "Harley-Davidson",
-		maxSpeed: 220,
-		price: 1400000,
-		image: "https://cdn.dealerspike.com/imglib/products/harley-showroom/2020/livewire/main/Vivid-Black-Main.png"
-	},
-	// это чтобы проверить работает ли default
-	// {
-	// 	id: 5,
-	// 	brand: "Harley-Davidson",
-	// 	maxSpeed: 220,
-	// 	price: 1400000,
-	// 	image: "https://png.pngtree.com/thumb_back/fw800/background/20230521/pngtree-d-green-bike-clipping-jpg-frame-of-a-swanky-green-electric-image_2665813.jpg"
-	// }
-];
-
-class Transport {
-	constructor(type, price, brand, image) {
-		this.type = type,
-		this.price = price,
-		this.brand = brand,
-		this.image = image
-	}
-
-	getInfo() {
-		let info = `${this.type} ${this.brand}`;
-		// console.log(info);
-		return info;
-	}
-
-	getPrice() {
-		let price = `${this.type} ${this.brand} стоит ${this.price}`;
-		// console.log(price);
-		return price;
+class Hero {
+	constructor(name, universe, alterego, occupation, friends, superpowers, url) {
+		(this.name = name), (this.universe = universe), (this.alterego = alterego), (this.occupation = occupation), (this.friends = friends), (this.superpowers = superpowers), (this.image = url);
 	}
 }
 
-class Car extends Transport {
-	constructor(type, price, brand, image, doors) {
-		super(type, price, brand, image, doors);
-		this.doors = doors;
-	}
-	getDoorsCount() {
-		let doors = `автомобиль ${this.brand} имеет ${this.doors} двери`;
-		// console.log(doors);
-		return doors;
-	}
-}
-class Bike extends Transport {
-	constructor(type, price, brand, image, maxSpeed) {
-		super(type, price, brand, image);
-		this.maxSpeed = maxSpeed;
-	}
+const addHero = () => {
+	heroes.forEach(item => {
+		const catalogue = document.getElementById("catalogue");
+		const newHero = document.createElement("div");
+		newHero.classList.add("hero");
+		catalogue.appendChild(newHero);
+		const newImage = document.createElement("img");
+		newImage.classList.add("image");
+		const newTitle = document.createElement("h2");
+		newTitle.classList.add("title");
+		const newDescription = document.createElement("div");
+		newDescription.classList.add("description");
+		const newRank = document.createElement("div");
+		newRank.classList.add("rank");
+		newRank.innerHTML = `<span class="star" data-value="1">&#9733;</span>
+<span class="star" data-value="2">&#9733;</span>
+<span class="star" data-value="3">&#9733;</span>
+<span class="star" data-value="4">&#9733;</span>
+<span class="star" data-value="5">&#9733;</span>`;
+		newHero.append(newTitle, newDescription, newImage, newRank);
 
-	getMaxSpeed() {
-		let speed = `Максимальная скорость мотоцикла ${this.brand}: ${this.maxSpeed} км/ч`;
-		// console.log(speed);
-		return speed;
-	}
-}
-
-const addVehicle = () => {
-data.forEach(item => {
-	const catalogue = document.getElementById("catalogue");
-	const newVehicle = document.createElement("div");
-	newVehicle.classList.add("vehicle");
-	catalogue.appendChild(newVehicle);
-	const newImage = document.createElement("img");
-	newImage.classList.add("image");
-	const newTitle = document.createElement("h2");
-	newTitle.classList.add("title");
-	const newPrice = document.createElement("div");
-	const newDescription = document.createElement("div");
-	newVehicle.append(newImage, newTitle, newPrice, newDescription);
-	switch (item.type) {
-	case "car":
-		let newCar = new Car("автомобиль", item.price, item.brand, item.image, item.doors);
-		newImage.src = newCar.image;
-		newTitle.textContent = newCar.getInfo();
-		newDescription.textContent = newCar.getDoorsCount();
-		newPrice.textContent = newCar.getPrice();
-		break;
-	case "bike":
-		let newBike = new Bike("мотоцикл", item.price, item.brand, item.image, item.maxSpeed);
-		newImage.src = newBike.image;
-		newTitle.textContent = newBike.getInfo();
-		newDescription.textContent = newBike.getMaxSpeed();
-		newPrice.textContent = newBike.getPrice();
-		break;
-	default:
-		// на тот случай, если в массиве появится неопределенный элемент
-        let newTransport = new Transport("транспортное средство", item.price, item.brand, item.image);
-		newImage.src = newTransport.image;
-		newTitle.textContent = newTransport.getInfo();
-		newPrice.textContent = newTransport.getPrice();
-		break;
-}
-})
+		let hero = new Hero(item.name, item.universe, item.alterego, item.occupation, item.friends, item.superpowers, item.url);
+		newImage.src = hero.image;
+		newImage.setAttribute("alt", item.name);
+		newTitle.textContent = item.name;
+		newDescription.innerHTML = `<div class="text">Вселенная: ${item.universe}</div>
+      <div class="text">Альтер-эго: ${item.alterego}</div>
+      <div class="text">Род деятельности: ${item.occupation}</div>
+      <div class="text">Друзья: ${item.friends}</div>
+      <div class="text">Суперсилы: ${item.superpowers}</div>`;
+	});
 };
-document.body.onload = addVehicle;
+addHero();
+
+
+const allHeroes = document.getElementsByClassName("hero");
+let allHeroesArray = [];
+
+console.log(allHeroesArray);
+let ratingsArray = JSON.parse(localStorage.getItem("rating")) || [];
+console.log(ratingsArray);
+
+const getRating = function() {
+	for (let hero of allHeroes) {
+	const rank = hero.querySelector(".rank");
+	const stars = rank.querySelectorAll(".star");
+	const name = hero.querySelector(".title").textContent;
+		for (let item of ratingsArray) {
+		if (item.id === name) {
+			for (let i = 0; i < stars.length; i++) {
+				if (i < item.rating) {
+					stars[i].classList.add("rated");
+					console.log(item.rating);
+				}
+		}
+	}}}}
+
+	getRating();
+
+for (let hero of allHeroes) {
+	const rank = hero.querySelector(".rank");
+	const stars = rank.querySelectorAll(".star");
+	const name = hero.querySelector(".title").textContent;
+	stars.forEach((star, index) => {
+		star.addEventListener("mouseenter", () => {
+			for (let i = 0; i <= index; i++) {
+				stars[i].classList.add("hovered");
+			}
+		});
+		star.addEventListener("mouseleave", () => {
+			for (let i = 0; i <= index; i++) {
+				stars[i].classList.remove("hovered");
+			}
+		});
+		star.addEventListener("click", e => {
+			let selected = e.target.getAttribute("data-value");
+			const i = ratingsArray.findIndex(e => e.id === name)
+			if (i > -1) {
+				ratingsArray[i] = {id: name, rating: selected};
+			}
+			else {
+				ratingsArray.push({id: name, rating: selected})
+			};
+			localStorage.setItem("rating", JSON.stringify(ratingsArray));
+			getRating();
+			console.log(ratingsArray);
+			for (let i = 0; i < stars.length; i++) {
+				if (i < selected) {
+					stars[i].classList.add("rated");
+				} else {
+					stars[i].classList.remove("rated");
+				}
+			}
+		});
+		})}
