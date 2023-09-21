b961c18ee65a64d25e49155031f9c90c81ca93cb
radionadejda <radionadejda@gmail.com>
2023-09-21 15:01:23 +0300

    add json post

--- itg_week_19/JSON POST/json_post.js
+++ itg_week_19/JSON POST/json_post.js
@@ -63,25 +63,13 @@ let inputTextValue = document.getElementById("inputText").value;
         }
     })
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => addPost(json))
     .catch((error) => {
         console.error("Ошибка", error);
     });
 }
 
+
+
 button.addEventListener('click', postPosts);
 
-function loadPosts() {
-        fetch("https://jsonplaceholder.typicode.com/posts")
-        .then((response) => response.json())
-        .then((posts) => {
-            posts.forEach((post) => {
-                addPost(post);
-            });
-        })
-        .catch((error) => {
-            console.error("Ошибка", error);
-        });
-}
-
-// loadPosts();
