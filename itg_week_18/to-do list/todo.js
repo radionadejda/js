ce913953dd16440a16767b3c3399457420e36cbf
radionadejda <radionadejda@gmail.com>
2023-10-07 16:05:13 +0300

    add todo

--- itg_week_18/to-do list/todo.js
+++ itg_week_18/to-do list/todo.js
@@ -8,6 +8,8 @@
 
 //     **Важно: Для сохранения состояния списка задач между сеансами работы с приложением используйте Local Storage. Это позволит восстановить список задач при повторном открытии приложения.**
 
+
+
 const taskInput = document.querySelector("#input");
 const buttonAdd = document.querySelector("#buttonAdd");
 const buttonClearAll = document.querySelector("#buttonClearAll");
@@ -15,19 +17,52 @@ const buttonClearDone = document.querySelector("#buttonClearDone");
 const taskList = document.querySelector("#tasks");
 const success = document.querySelector("#success");
 let taskText = taskInput;
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-const printTask = function(taskText) {
-	const newTaskItem = document.createElement("label");
-	newTaskItem.classList.add("task");
-	newTaskItem.innerHTML = `
-	<p>${taskText}</p>
-	<input type="checkbox" name="task-checkbox"  unchecked class="checkbox"/>`;
+// класс для создания задач
+class Task {
+	constructor(taskText, done = false) {
+	this.taskText = taskText;
+	this.done = done;
+	this.printTask();
+	}
+
+	printTask() {
+		const newTaskItem = document.createElement("label");
+		newTaskItem.classList.add("task");
+		newTaskItem.innerHTML = `
+		<p>${this.taskText}</p>
+		<input type="checkbox" name="task-checkbox"  ${this.done ? "checked" : ""} class="checkbox"/>`;
+		const checkbox = newTaskItem.querySelector(".checkbox");
+		checkbox.addEventListener("change", () => {
+		this.done = checkbox.checked;
+		localStorage.setItem("tasks", JSON.stringify(tasks));
+		enableButtonClearDone();
+	});
+	console.log(`i made task ${this.taskText}`);
 	taskList.appendChild(newTaskItem);
-	taskInput.value = "";
+		taskInput.value = "";
+	}
+
 }
 
-let buttonClearAllEnable = function() {
+// выводим сохраненные задачи (печатаются за счет вызова метода print в классе)
+const getTasks = function() {
+	for (let task of tasks) {
+		const newTask = new Task(task.taskText, task.done);
+		const newTaskItem = taskList.lastChild;
+    if (task.done) {
+		const checkbox = newTaskItem.querySelector(".checkbox");
+		checkbox.checked = true;
+    }
+	}
+	applyCheckboxes();
+	enableButtonClearAll();
+	enableButtonClearDone();
+};
+
+// включает/выключает кнопку удалить все
+const enableButtonClearAll = function() {
 	if (tasks.length !== 0) {
 		buttonClearAll.disabled = false;
 		success.style.display = "none";
@@ -37,29 +72,19 @@ let buttonClearAllEnable = function() {
 	}
 };
 
+// удаляет все
 const clearAll = function() {
 	let tasksOnPage = taskList.querySelectorAll(".task");
 	tasksOnPage.forEach(task => task.remove());
 	tasks.length = 0;
 	window.localStorage.removeItem("tasks")
-	buttonClearAllEnable();
+	enableButtonClearAll();
 }
 
-const getTasks = function() {
-	for (let task of tasks) {
-		console.log(task);
-		printTask(task);
-	};
-	buttonClearAllEnable();
-};
+// запускает функцию удаления всех задач
+buttonClearAll.addEventListener("click", clearAll);
 
-if (tasks.length !== 0) {
-	getTasks()
-} else {
-	clearAll()
-}
-
-// проверяет ввод и добавляет задачу
+// проверяет ввод пользователя и добавляет задачу
 const addTask = function() {
 	taskText = taskInput.value.trim();
 	if (taskText === "") {
@@ -67,10 +92,10 @@ const addTask = function() {
 		return;
 	} else {
 		taskText = taskInput.value.trim();
-		printTask(taskText);
-		tasks.push(taskText);
+		let newTask = new Task(taskText);
+		tasks.push(newTask);
 		localStorage.setItem("tasks", JSON.stringify(tasks));
-		buttonClearAllEnable();
+		enableButtonClearAll();
 	}
 };
 
@@ -88,5 +113,59 @@ taskInput.addEventListener("keypress", function(event) {
 	}
 });
 
-// удалить все задачи
-buttonClearAll.addEventListener("click", clearAll);
+
+
+// вкл/выкл кнопку удаления выполненных задач
+function enableButtonClearDone() {
+	const checkboxes = document.querySelectorAll(".checkbox");
+	for (const checkbox of checkboxes) {
+	if (checkbox.checked) {
+		buttonClearDone.disabled = false;
+		return;
+	} else {buttonClearDone.disabled = true;}
+	}
+	if (tasks.length == 0) {buttonClearDone.disabled = true;}
+}
+
+// удаляет выполненные задачи
+function clearDoneTasks() {
+	const checkboxes = document.querySelectorAll(".checkbox");
+	for (const checkbox of checkboxes) {
+	if (checkbox.checked) {
+		const taskItem = checkbox.closest(".task");
+		const index = tasks.findIndex(task => task.taskText === taskItem.textContent.trim());
+		if (index !== -1) {
+		tasks.splice(index, 1);
+		taskItem.remove();
+		}
+	}
+	}
+	localStorage.setItem("tasks", JSON.stringify(tasks));
+	enableButtonClearAll();
+	enableButtonClearDone();
+}
+
+// запускает функцию удаления выполненных задач
+buttonClearDone.addEventListener("click", clearDoneTasks);
+
+// при загрузке проверяет, есть ли сохраненные задачи и выводит имеющиеся либо очищает все
+window.onload = function() {
+    if (tasks.length !== 0) {
+		getTasks()
+	} else {
+		clearAll()
+	}
+}
+
+function applyCheckboxes() {
+const checkboxes = document.querySelectorAll(".checkbox");
+	for (const checkbox of checkboxes) {
+		checkbox.addEventListener("change", () => {
+			const taskItem = checkbox.closest(".task");
+			const index = tasks.findIndex(task => task.taskText === taskItem.textContent.trim());
+			tasks[index].done = checkbox.checked;
+			localStorage.setItem("tasks", JSON.stringify(tasks));
+			enableButtonClearDone();
+		});
+	}
+}
