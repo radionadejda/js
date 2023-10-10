ee0eed78d433ae5009fc432ec5ce9cbc309dd593
radionadejda <radionadejda@gmail.com>
2023-10-10 11:22:53 +0300

    add task print

--- itg_week_20/dashboard/source/assets/js/dashboard.js
+++ itg_week_20/dashboard/source/assets/js/dashboard.js
@@ -4,8 +4,101 @@ import ru from "moment/locale/ru.js";
 import Chart from "chart.js/auto";
 
 // сделать массив с задачами, который в JSON должен быть
-// я понимаю, что правильно было бы реализовать возможность добавления и изменения задач пользователем, но не успеваю за одну неделю, и задание скорее на отработку подключения модулей.
-// там же потянется обработка некорректного ввода, вывод ошибок, обновление массива и обновление вывода на страницу.
+let tasks = [
+	{
+		"title": "задача 1",
+		"status": "в процессе",
+		"priority": "высокий",
+		"startDate": "2023-10-01",
+		"endDate": "2023-10-10"
+	},
+	{
+		"title": "задача 2",
+		"status": "выполнено",
+		"priority": "высокий",
+		"startDate": "2023-10-01",
+		"endDate": "2023-10-05"
+	},
+	{
+		"title": "задача 3",
+		"status": "в процессе",
+		"priority": "низкий",
+		"startDate": "2023-10-10",
+		"endDate": "2023-11-10"
+	},
+	{
+		"title": "задача 4",
+		"status": "выполнено",
+		"priority": "средний",
+		"startDate": "2023-09-01",
+		"endDate": "2023-10-01"
+	},
+	{
+		"title": "задача 5",
+		"status": "в процессе",
+		"priority": "высокий",
+		"startDate": "2023-10-01",
+		"endDate": "2023-10-10"
+	},
+]
+
+const taskList = document.getElementById("taskList")
+
+// task making class
+class Task {
+	constructor(title, status, priority, startDate, endDate) {
+	this.title = title;
+	this.status = status;
+	this.priority = priority;
+	this.startDate = startDate;
+	this.endDate = endDate;
+	this.printTask();
+	}
+
+	printTask() {
+		const newTask = document.createElement("div");
+		newTask.classList.add("task");
+		// заголовок задачи
+		const title = document.createElement("h3");
+		title.textContent = `Заголовок: ${this.title}`;
+		title.classList.add("h3");
+		newTask.appendChild(title);
+		// task options
+		const taskOptions = document.createElement("div");
+		taskOptions.classList.add("task-options");
+		newTask.appendChild(taskOptions);
+		// options > priority
+		const priority = document.createElement("div");
+		priority.textContent = `приоритет: ${this.priority}`;
+		taskOptions.appendChild(priority);
+		// options > status
+		const status = document.createElement("div");
+		status.textContent = `статус: ${this.status}`;
+		taskOptions.appendChild(status);
+		// task dates
+		const taskDates = document.createElement("div");
+		taskDates.classList.add("task-dates");
+		newTask.appendChild(taskDates);
+		// dates > start date
+		const startDate = document.createElement("div");
+		startDate.textContent = `дата начала: ${this.startDate}`;
+		taskDates.appendChild(startDate);
+		// options > status
+		const endDate = document.createElement("div");
+		endDate.textContent = `дата окончания: ${this.endDate}`;
+		taskDates.appendChild(endDate);
+	console.log(`i made task ${this.taskText}`);
+	taskList.appendChild(newTask);
+	}
+}
+
+const getTasks = function() {
+	for (let task of tasks) {
+		const newTask = new Task(task.title, task.status, task.priority, task.startDate, task.endDate);
+	}
+}
+
+getTasks();
 
 // making chart
 const context = document.getElementById("chart").getContext("2d");
@@ -80,5 +173,29 @@ function updateClock() {
 	let formattedTime = currentTime.format('DD/MM/YYYY dddd HH:mm:ss');
 	date.textContent = formattedTime;
 }
+// setInterval(updateClock, 1000);
 
-setInterval(updateClock, 1000);
+// making time counter in footer
+const counter = document.getElementById('counter');
+const start = moment();
+function calculateTime () {
+        const end = moment();
+        let seconds = Math.round((end - start) / 1000);
+		let minutes = 0;
+		let hours = 0;
+		if (seconds>60) {
+			minutes = Math.round(seconds/60);
+			seconds = seconds%60;
+		}
+		if (minutes>60) {
+			hours = Math.round(minutes/60);
+			minutes = Math.round(seconds/60);
+			seconds = seconds%60;
+		}
+		let timeSpent = `${hours} часов ${minutes} минут ${seconds} секунд`
+		counter.textContent = timeSpent;
+    };
+
+// setInterval(calculateTime, 1000);
+
+// note to self: правильно было бы реализовать возможность добавления и изменения задач пользователем, но не успеваю за одну неделю, и задание скорее на отработку подключения модулей. там потянется обработка некорректного ввода, вывод ошибок, обновление массива и обновление вывода на страницу.
